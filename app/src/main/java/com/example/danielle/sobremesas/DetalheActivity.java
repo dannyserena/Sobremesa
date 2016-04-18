b0dc682dfda188d5564ce9508144206b4b0762ca
dannyserena <dannyserena@gmail.com>
2016-04-18 00:21:08 -0300

    xml de doces editado

--- app/src/main/java/com/example/danielle/sobremesas/DetalheActivity.java
+++ app/src/main/java/com/example/danielle/sobremesas/DetalheActivity.java
@@ -30,14 +30,26 @@ public class DetalheActivity extends AppCompatActivity {
       //  if(imageView == id){
             imageView.setImageResource(R.drawable.morango);
            // imageView.setImageResource(R.drawable.id.imageView);
-            textViewIngredientes.setText("Acucar, farinha");
-            textViewModoPreparo.setText("Misture os ingredientes");
+            textViewIngredientes.setText("" +
+                    "                    2 colheres ( sopa) de margarina \n"+
+                    "                    2 colheres (sopa) de óleo\n" +
+                    "                    1 ovo inteiro\n" +
+                    "                    2 colheres rasas (sopa) de açúcar\n" +
+                    "                    1 pitada de sal\n" +
+                    "                    1 colher rasa (sopa) de fermento em pó\n" +
+                    "                    Farinha de trigo até a massa desgrudar da mão.");
+            textViewModoPreparo.setText("Em primeiro lugar faça a gelatina conforme o modo de preparo, e deixe-a na geladeira porque na hora do uso ela deve estar em ponto de clara de ovo pois se estiver líquida, a gelatina penetrará na massa, depois faça o creme, junte todos os ingredientes e mexa até levantar fervura e você ver que ele está bem cremoso, cuidado para não deixar empelotar ou grudar no fundo da panela, mexa bastante sem parar. Na hora do uso ele deve estar já frio ou levemente morno, agora faça a massa junte todos os ingredientes e quando ela estiver no ponto forre um pirex de médio a grande, os mais fundos são melhores fure a massa com um garfo e asse em fogo médio entre 10 a 15 minutos, quando dourar está boa, cuidado para não deixar queimar pois a massa fica fina.\n" +
+                    "Para a Montagem:  Massa assada no pirex, deixe esfriar um pouco coloque o creme\n" +
+                    "Os morangos cortados na transversal, coloque eles sobre todo o creme, agora coloque a gelatina já em ponto de clara, assim ela ficará na superfície\n" +
+                    "Levar à geladeira por no mínimo 4 horas.");
 
             String valorSobremesa = getIntent().getExtras().getString("sobremesa");
 
-        if (valorSobremesa.equals("crepe")) {
+        if (valorSobremesa.equals("btnCrepe")) {
 
         } else if (valorSobremesa.equals("doces")) {
+
+        } else if(valorSobremesa.equals("doces")){
 
         }
 
